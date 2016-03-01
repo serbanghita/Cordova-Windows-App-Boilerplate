1b5dbf3933c927eff9f9f60fca634ee811f7cfa0
Serban Ghita <sghita@adobe.com>
2016-03-01 16:45:50 +0200

    Added sample/demo typescript files.

---
+++ src/MyController.ts
@@ -0,0 +1,15 @@
+﻿import {MyView} from "./MyView";
+
+export class MyController {
+    protected view: MyView;
+
+    public constructor(view: MyView) {
+        this.view = view;
+    }
+
+    public startAction() {
+        var msg = 'starting from MyController.';
+        alert(msg);
+        console.log(msg);
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+﻿import {MyController} from "./MyController";
+import {MyView} from "./MyView";
+
+var c = new MyController(new MyView());
+c.startAction();
